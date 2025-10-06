6b082a16a8736cec3e86a07cbd8ce5e3c7abee8a
tonado-developer <support@tonado.net>
2025-10-06 13:51:32 +0200

    changed version

--- wp-content/themes/sws2025/blocks/image-mapper/frontend.js
+++ wp-content/themes/sws2025/blocks/image-mapper/frontend.js
@@ -3,7 +3,7 @@
  * WordPress Image Mapper Frontend with GSAP - Extended Version
  * 
  * @description Interactive image mapper with global elements support
- * @version 3.0.1
+ * @version 3.0.2
  * @requires GSAP 3.x
  * 
  * Features:
